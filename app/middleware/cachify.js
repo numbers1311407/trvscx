7a1841f58fb70a8af4abf2705cf8fce35a9f67b7
Travis <numbers1311407@gmail.com>
2013-10-07 19:23:35 -0400

    cachify cdn prefix

--- app/middleware/cachify.js
+++ app/middleware/cachify.js
@@ -5,6 +5,7 @@ module.exports = function (isdev) {
   return cachify.setup(config.assets, {
     root: process.cwd() + "/public",
     url_to_paths: cachify.url_to_paths,
-    production: !isdev
+    production: !isdev,
+    prefix: "http://cdn.trvscx.com/"
   })
 }
