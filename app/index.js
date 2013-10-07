654443b575c725dd0d0923f885dcb72184282c54
Travis <numbers1311407@gmail.com>
2013-10-07 09:55:24 -0400

    implement express-cdn

--- app/index.js
+++ app/index.js
@@ -12,17 +12,44 @@ var app = module.exports = express()
 
 var isdev = 'production' !== process.env.NODE_ENV
 
+// Set the CDN options
+var options = {
+    publicDir  : path.join(__dirname, '../public')
+  , viewsDir   : path.join(__dirname, 'views')
+  , domain     : 'cdn.trvscx.com'
+  , bucket     : 'cdn.trvscx'
+  , endpoint   : 'cdn.trvscx.s3.amazonaws.com' // optional
+  , key        : process.env.AWS_KEY
+  , secret     : process.env.AWS_SECRET
+  , hostname   : 'localhost'
+  , port       : process.env.PORT || 3000
+  , ssl        : false
+  , production : !isdev
+};
+
+// Hack to allow express-cdn to upload to AWS unsecurely.  This, I
+// believe, is a bug because the bucket name has a dot in it.  It
+// is apparently fixed in later versions of knox which express-cdn is
+// not using yet.
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
+// Initialize the CDN magic
+var CDN = require('express-cdn')(app, options);
+
+app.locals({ CDN: CDN() });
+
 app.configure(function () {
   app.set('port', process.env.PORT || 3000)
   app.set('views', __dirname + '/views')
   app.set('view engine', 'jade')
+
   app.use(express.favicon(__dirname + '/../public/favicon.ico'));
   app.use(express.logger('dev'));
   app.use(express.compress());
 
   app.locals.pretty = isdev;
 
-  app.use(require("./middleware/cachify")(isdev));
+  // app.use(require("./middleware/cachify")(isdev));
 
   if (isdev) {
     app.use(require("less-middleware")({
